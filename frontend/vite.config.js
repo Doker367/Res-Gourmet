db6f6e46168df86afcaf73fb8a45b08660daa158
Doker367 <alberto.grajales@unach.mx>
2026-04-23 10:57:40 -0600

    Dokploy

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    base: '/res-gourmet/',
     plugins: [react()],
     server: {
         port: 5173,
